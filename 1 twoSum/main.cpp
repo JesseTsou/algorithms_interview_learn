63480f3fef5465a35db1a66a882ae0109a4b53f2
Jesse <693939576@qq.com>
2018-08-08 01:46:14 +0800

    【1】添加一遍hash解法

--- 1 twoSum/main.cpp
+++ 1 twoSum/main.cpp
@@ -26,9 +26,11 @@ public:
         throw;
     }
     /*
+     * 两遍hash
      *使用hash map来存储键值对，然后再搜索target - val是否存在
      * unordered_map 在C++ 11被引标准库，而hash_map没有，建议使用unordered_map
      * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
      */
     vector<int> twoSum_map1(vector<int>& nums, int target) {
         unordered_map<int,int> mymap;
@@ -50,6 +52,29 @@ public:
         }
         throw;
     }
+    /*
+     * 一遍hash
+     * 只使用一次循环，先进行查询再插入数值
+     * 时间复杂度：O（n）
+     * 空间复杂度：O(n)
+     */
+    vector<int> twoSum_map2(vector<int>& nums, int target) {
+        unordered_map<int,int> mymap;
+        vector<int>  ret;
+
+        for (int i = 0; i < nums.size(); i ++)
+        {
+            int sec = target - nums[i];
+            if (mymap.find(sec) != mymap.end() && i != mymap[sec] )
+            {
+                ret.push_back(mymap[sec]);
+                ret.push_back(i);
+                return ret;
+            }
+            mymap[nums[i]] = i;
+        }
+        throw;
+    }
 };
 int main() {
     int nums[] = {3,2,4};
@@ -61,6 +86,10 @@ int main() {
     cout << ret[0] << "," << ret[1] << endl;
 
     ret = sl.twoSum_map1(val, 6);
+
+    cout << ret[0] << "," << ret[1] << endl;
+
+    ret = sl.twoSum_map2(val, 6);
 
     cout << ret[0] << "," << ret[1] << endl;
 
