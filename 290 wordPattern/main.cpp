55c5336446c84926636be8e66140abbb6fb9422b
Jesse <693939576@qq.com>
2018-08-19 23:48:46 +0800

    【290 wordPattern】添加另一种比较好的方法

--- 290 wordPattern/main.cpp
+++ 290 wordPattern/main.cpp
@@ -11,6 +11,9 @@ using  namespace std;
 class Solution {
 public:
 
+    /*
+     * 获取以空格分隔的元素
+     */
     vector<string> getvec(string str, string tar)
     {
         vector<string> vec;
@@ -28,9 +31,13 @@ public:
         return vec;
     }
 
+    /*
+     * 时间复杂度：O(nlogn)
+     * 需要建立两个map，键值分别是pattern与str中的元素，需要保证一一对应
+     */
     bool wordPattern(string pattern, string str) {
-        unordered_map<int,string> map1;
-        unordered_map<string,int> map2;
+        unordered_map<int,string> map1;//pattern->str
+        unordered_map<string,int> map2;//str->parrern
         vector<string> vec = getvec(str, " ");
 
         if (pattern.size() != vec.size()){
@@ -60,14 +67,47 @@ public:
 
         return true;
     }
+    /*
+     * 时间复杂度：O(nlogn)
+     * 三个map，前两个保存出现的次数，第三个保存对应关系
+     *
+     */
+    bool wordPattern2(string pattern, string str) {
+        unordered_map<char,int> map1;//字母及出现的次数
+        unordered_map<string,int> map2;//单词及出现的次数
+        unordered_map<char,string> map3;//对应关系
+
+        vector<string> vec = getvec(str, " ");
+
+        if (pattern.size() != vec.size()){
+            return false;
+        }
+
+        for (int i = 0; i < pattern.size(); i++)
+        {
+            if (map1[pattern[i]] == 0){
+                if (map2[vec[i]] == 0){
+                    map1[pattern[i]] ++;
+                    map2[vec[i]] ++;
+                    map3[pattern[i]] = vec[i];
+                } else
+                    return false;
+            }else{
+                if (map3[pattern[i]] != vec[i])
+                    return false;
+            }
+        }
+
+        return true;
+    }
 };
 
 int main() {
     string str1 = "abba";
-    string str2 = "dog dog dog dog";
+    string str2 = "dog zou zou dog";
 
     Solution s;
-    if (s.wordPattern(str1, str2)){
+    if (s.wordPattern2(str1, str2)){
         cout << "is match" << endl;
     }else{
         cout << "is not match" << endl;
