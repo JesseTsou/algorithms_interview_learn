bbf36ea84c87cab8db540cb934583624759a09fd
JesseTsou <693939576@qq.com>
2018-08-26 11:53:08 +0800

    【149 maxPointsOnALine】添加第二种解法，使用dx/dy做判断，思路基本与第一种相同

--- 149 maxPointsOnALine/main.cpp
+++ 149 maxPointsOnALine/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <map>
 using namespace std;
 /*
  * 给定一个二维平面，平面上有 n 个点，求最多有多少个点在同一条直线上。
@@ -54,6 +55,59 @@ public:
         }
         return res;
     }
+    /*
+     * 时间复杂度：O(n^2)
+     * 使用unordered_map保存化简后的dx/dy以及对应的次数
+     * 相同的dx/dy即在同一个直线上
+     * 注意：需要加上对相同点的处理
+     *      最大公约数的计算
+     */
+    int maxPoints2(vector<Point>& points) {
+        int res = 0;
+        for (int i = 0; i < points.size(); i ++) {
+            int samenum = 0;
+            map<pair<int,int>, int> map1;
+            for (int j = 0; j < points.size(); j++)
+            {
+                //对相同点的判断
+                if (points[i].y == points[j].y && points[i].x == points[j].x) {
+                    samenum ++;
+                }
+                else{
+                    map1[getpair(points[i], points[j])] ++;
+                }
+            }
+            //对只有相同点的情况进行处理
+            res = max(res,samenum);
+            //遍历map，取最大值，需要加上相同点
+            for (map<pair<int,int>, int>::iterator iter = map1.begin(); iter != map1.end(); iter ++)
+            {
+                res = max(res, iter->second + samenum);
+            }
+        }
+        return res;
+    }
+
+    //求化简后的dx/dy
+    pair<int,int> getpair(Point a, Point b)
+    {
+        int dx = a.x - b.x;
+        int dy = a.y - b.y;
+
+        //对x或y相等的判断
+        if (dx == 0)
+            return make_pair(0,1);
+        if (dy == 0)
+            return make_pair(1,0);
+
+        int gcd = getgcd(dx, dy);
+        dx /= gcd;
+        dy /= gcd;
+
+        return make_pair(dx, dy);
+    }
+    //求最大公约数
+    int getgcd(int a, int b){ return a == 0 ? b : getgcd(b % a, a); }
 };
 int main() {
     Point p1(1,1);
@@ -70,7 +124,7 @@ int main() {
     points.push_back(p5);
     points.push_back(p6);
     Solution s;
-    int ret = s.maxPoints(points);
+    int ret = s.maxPoints2(points);
     cout << ret << endl;
     return 0;
 }
