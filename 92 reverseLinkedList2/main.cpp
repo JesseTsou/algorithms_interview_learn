fa05955435309229d966fb75f01697184a8a658e
Jesse <693939576@qq.com>
2018-08-30 01:18:51 +0800

    【92 reverseLinkedList2】添加一种解法，有些繁琐

--- 92 reverseLinkedList2/main.cpp
+++ 92 reverseLinkedList2/main.cpp
@@ -12,8 +12,55 @@ struct ListNode {
 
 class Solution {
 public:
+    /*
+     * 时间复杂度：O(n)
+     * 对于m=1的情况需要作特殊处理。
+     */
     ListNode* reverseBetween(ListNode* head, int m, int n) {
+        ListNode *pm_pre = head;
 
+        //记录m前一个结点
+        for (int i = 1; i < m - 1; i ++){
+            pm_pre = pm_pre->next;
+        }
+
+        //创建虚拟头结点，其下一个结点为m位置结点
+        ListNode *newhead = new ListNode(0);
+        if (m==1)
+            newhead->next = pm_pre;
+        else
+            newhead->next = pm_pre->next;
+
+        //反转m至n的结点
+        ListNode *pre = newhead;
+        ListNode *cur = newhead->next;
+        ListNode *next = NULL;
+        ListNode *temp = cur;//m结点位置
+        for (int i = m; i <= n; i ++){
+            if (cur == NULL)
+                break;
+            next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        if (m != 1)
+        {
+            //将m前一个结点的next指向反转后的第一个结点(即n结点)
+            //将m结点的next指向原来n的下一个结点
+            if (pm_pre->next){
+                pm_pre->next = pre;
+                temp->next = cur;
+            }
+        }else{
+            //若m==1，即n结点即头结点
+            //将m结点的next指向原来n的下一个结点
+            head = pre;
+            temp->next = cur;
+        }
+        delete newhead;
+        return head;
     }
 };
 
@@ -59,11 +106,12 @@ int delList(ListNode *head)
 
 int main() {
     int arr[] = {1,2,3,4,5};
+    //int arr[] = {3,5};
     int n = sizeof(arr)/sizeof(arr[0]);
     ListNode *head = createList(arr,n);
     printList(head);
     Solution s;
-    ListNode * head2 = s.reverseBetween(head,2,2);
+    ListNode * head2 = s.reverseBetween(head,2,4);
     printList(head2);
     delList(head2);
     return 0;
