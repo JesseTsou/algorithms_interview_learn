2f983ca4fea4b0564d90ec16570b335e395f80f7
JesseTsou <693939576@qq.com>
2018-08-31 06:44:05 +0800

    【92 reverseLinkedList2】添加第二种解法

--- 92 reverseLinkedList2/main.cpp
+++ 92 reverseLinkedList2/main.cpp
@@ -62,6 +62,32 @@ public:
         delete newhead;
         return head;
     }
+
+    /*
+     * 时间复杂度：O(n)
+     * 创建虚拟头结点
+     * 先将m的下一点指向自身，最后再处理m的下一个结点
+     */
+    ListNode* reverseBetween2(ListNode* head, int m, int n) {
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *pre = dummy;
+        //m点前一点pre
+        for (int i = 1; i <=m - 1; i ++){
+            pre = pre->next;
+        }
+        ListNode *cur = pre->next;
+        ListNode *pm = cur;
+        ListNode *next = NULL;
+        for (int i = m; i <= n; i ++){
+            next = cur->next;//记录当前点的下一点
+            cur->next = pre->next;//对于m点，指向自身；对于（m,n]指向其前一点
+            pre->next = cur;//记录当前点，即下一点的前一点
+            cur = next;//当前点移动到下一点
+        }
+        pm->next = cur;//m点下一结点指向n的下一结点
+        return dummy->next;//dummy点始终未变
+    }
 };
 
 ListNode* createList(int arr[], int n)
@@ -111,7 +137,7 @@ int main() {
     ListNode *head = createList(arr,n);
     printList(head);
     Solution s;
-    ListNode * head2 = s.reverseBetween(head,2,4);
+    ListNode * head2 = s.reverseBetween2(head,1,2);
     printList(head2);
     delList(head2);
     return 0;
