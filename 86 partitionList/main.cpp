e8491ac2ca28f822f0b07bd1530ead6485524973
JesseTsou <693939576@qq.com>
2018-09-01 12:15:26 +0800

    【86 partitionList】添加一种解法

---
+++ 86 partitionList/main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+/*
+ *给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+ * 你应当保留两个分区中每个节点的初始相对位置。
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    /*
+     * 时间复杂度：O(n)
+     * 记录最后一低点小于x的点less
+     * 遍历链表，当遍历到的点(pre)的下一个点(cur)小于x时，将下一点(cur)插入到less之后，less相应向后移动一位
+     * 并且pre需要指向原cur的下一个点（next），保证链表不会断，所以要以pre来遍历
+     * 特殊情况：当满足条件时，当less与当前点前一点重合时，只需要向后移动一位即可
+     */
+    ListNode* partition(ListNode* head, int x) {
+        if (head == NULL)
+            return NULL;
+        ListNode *dummy = new ListNode(0);
+
+        dummy->next = head;
+
+        ListNode *pre = dummy;
+        ListNode *less = dummy;
+        ListNode *cur = NULL;
+        ListNode *less_next = NULL;
+        ListNode *cur_next = NULL;
+
+        while(pre->next){
+            cur = pre->next;
+            if (cur->val < x){
+                if (pre == less){
+                    less = less->next;
+                    pre = pre->next;
+                    continue;
+                }
+                less_next = less->next;
+                cur_next = cur->next;
+                cur->next = less_next;
+                less->next = cur;
+                pre->next = cur_next;
+                less = less->next;
+            } else
+                pre = pre->next;
+        }
+        return dummy->next;
+    }
+};
+
+ListNode* createList(int arr[], int n)
+{
+    if (n == 0)
+        return NULL;
+    ListNode *head = new ListNode(arr[0]);
+
+    ListNode *curNode = head;
+
+    for (int i = 1; i < n; i ++){
+        curNode->next = new ListNode(arr[i]);
+        curNode = curNode->next;
+    }
+    return head;
+}
+
+int printList(ListNode *head)
+{
+    ListNode *curNode = head;
+
+    while(curNode != NULL){
+        cout << curNode->val <<" -> ";
+        curNode = curNode->next;
+    }
+    cout << "NULL" << endl;
+    return 0;
+}
+
+int delList(ListNode *head)
+{
+    if (head == NULL)
+        return 0;
+    ListNode *curNode = head;
+    while(curNode != NULL){
+        ListNode *delNode = curNode;
+        curNode = curNode->next;
+        delete delNode;
+    }
+    return 0;
+}
+
+int main() {
+    int arr[] = {1,4,3,2,5,2};
+    //int arr[] = {1,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    ListNode *head = createList(arr,n);
+    printList(head);
+    Solution s;
+    ListNode * head2 = s.partition(head,3);
+    printList(head2);
+    delList(head2);
+    return 0;
+}
