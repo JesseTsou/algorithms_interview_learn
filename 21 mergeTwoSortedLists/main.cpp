a22612cc637ed9f7a539bcb5bb5a359fa647ce2b
JesseTsou <693939576@qq.com>
2018-09-02 12:14:08 +0800

    【21 mergeTwoSortedLists】添加递归实现

--- 21 mergeTwoSortedLists/main.cpp
+++ 21 mergeTwoSortedLists/main.cpp
@@ -40,6 +40,25 @@ public:
 
         return dummy->next;
     }
+
+    /*
+     * 递归实现
+     */
+    ListNode* mergeTwoLists2(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL)
+            return l2;
+        if (l2 == NULL)
+            return l1;
+
+        if (l1->val < l2->val){
+            l1->next = mergeTwoLists2(l1->next, l2);
+            return l1;
+        } else{
+            l2->next = mergeTwoLists2(l1, l2->next);
+            return l2;
+        }
+        return NULL;
+    }
 };
 
 ListNode* createList(int arr[], int n)
@@ -91,7 +110,7 @@ int main() {
     printList(head1);
     printList(head2);
     Solution s;
-    ListNode * head3 = s.mergeTwoLists(head1,head2);
+    ListNode * head3 = s.mergeTwoLists2(head1,head2);
     printList(head3);
     delList(head1);
     return 0;
